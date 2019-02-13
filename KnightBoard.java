b0e967aed40b09ac3e25c9e791d1ec75a4684d77
Jason Zheng <zzheng3@stuy.edu>
2019-02-13 14:27:14 -0500

    Started work on class.

---
+++ KnightBoard.java
@@ -0,0 +1,32 @@
+public class KnightBoard {
+  @throws IllegalArgumentException when either parameter is <= 0.
+public KnightBoard(int startingRows,int startingCols)
+
+    Initialize the board to the correct size and make them all 0's
+
+
+public String toString()
+see format for toString below
+blank boards display 0's as underscores
+you get a blank board if you never called solve or
+when there is no solution
+
+Modifies the board by labeling the moves from 1 (at startingRow,startingCol) up to the area of the board in proper knight move steps.
+@throws IllegalStateException when the board contains non-zero values.
+@throws IllegalArgumentException when either parameter is negative
+ or out of bounds.
+@returns true when the board is solvable from the specified starting position
+public boolean solve(int startingRow, int startingCol)
+
+
+
+@throws IllegalStateException when the board contains non-zero values.
+@throws IllegalArgumentException when either parameter is negative
+ or out of bounds.
+@returns the number of solutions from the starting position specified
+public int countSolutions(int startingRow, int startingCol)
+
+Suggestion:
+private boolean solveH(int row ,int col, int level)
+// level is the # of the knight
+}
